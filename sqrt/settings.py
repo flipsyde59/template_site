563c112de0036efc4b435e82f4eeb879b7f05c79
Arhtur Miller <66296960+flipsyde59@users.noreply.github.com>
2020-12-10 15:18:43 +0500

    Update settings.py

--- sqrt/settings.py
+++ sqrt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9)3g!ve)#g4ef6s_q-)9mhb7_a)5^*b#ao+nr0ilo-%a&!r3gk'
+SECRET_KEY = 'secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
